1598985974837dc7e869a043aca7f62506296a40
Jacob Webb <jakewebb4@utexas.edu>
2015-09-07 14:04:35 -0500

    Corrected the colors

--- project1/simpletriangle.cpp
+++ project1/simpletriangle.cpp
@@ -1,43 +1,40 @@
 #include <GL/glut.h>  // includes necessary OpenGL headers
 
-void display() {
-    glShadeModel(GL_SMOOTH);  // smooth color interpolation
-    glEnable(GL_DEPTH_TEST);  // enable hidden surface removal
-    
+void display() { 
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-    glColor4ub(238, 118, 0, 255);    // RGBA=(1,0,0,100%)
+    glColor4ub(0, 0, 0, 255);  
     glBegin(GL_TRIANGLES);  // every 3 vertexes makes a triangle
-        glVertex3f(-0.9,  0.8,  0);  // XYZ=(-8/10,8/10,3/10)
-        glVertex3f(-0.75,  0.8, 0);  // XYZ=(8/10,8/10,-2/10)
-        glVertex3f(-0.45, -0.8, 0);  // XYZ=(0,-8/10,-2/10)
+        glVertex3f(-0.9,  0.8, 0); 
+        glVertex3f(-0.75,  0.8, 0);  
+        glVertex3f(-0.45, -0.8, 0); 
 
-        glVertex3f(-0.75,  0.8,  0);  // XYZ=(-8/10,8/10,3/10)
-        glVertex3f(-0.425, -0.5, 0);  // XYZ=(8/10,8/10,-2/10)
-        glVertex3f(-0.45, -0.8, 0);  // XYZ=(0,-8/10,-2/10)
+        glVertex3f(-0.75,  0.8, 0); 
+        glVertex3f(-0.425, -0.5, 0); 
+        glVertex3f(-0.45, -0.8, 0);  
 
-        glVertex3f(0, -0.1,  0);  // XYZ=(-8/10,8/10,3/10)
-        glVertex3f(-0.425, -0.5, 0);  // XYZ=(8/10,8/10,-2/10)
-        glVertex3f(-0.45, -0.8, 0);  // XYZ=(0,-8/10,-2/10)
+        glVertex3f(0, -0.1, 0); 
+        glVertex3f(-0.425, -0.5, 0);  
+        glVertex3f(-0.45, -0.8, 0);  
 
-        glVertex3f(0, -0.1,  0);  // XYZ=(-8/10,8/10,3/10)
-        glVertex3f(-0.425, -0.5, 0);  // XYZ=(8/10,8/10,-2/10)
-        glVertex3f(0, 0.1, 0);  // XYZ=(0,-8/10,-2/10)
+        glVertex3f(0, -0.1, 0); 
+        glVertex3f(-0.425, -0.5, 0); 
+        glVertex3f(0, 0.1, 0);  
 
-        glVertex3f(0, -0.1,  0);  // XYZ=(-8/10,8/10,3/10)
-        glVertex3f(0.45, -0.8, 0);  // XYZ=(8/10,8/10,-2/10)
-        glVertex3f(0, 0.1, 0);  // XYZ=(0,-8/10,-2/10)
+        glVertex3f(0, -0.1, 0); 
+        glVertex3f(0.45, -0.8, 0); 
+        glVertex3f(0, 0.1, 0); 
 
-        glVertex3f(0.425, -0.5,  0);  // XYZ=(-8/10,8/10,3/10)
-        glVertex3f(0.45, -0.8, 0);  // XYZ=(8/10,8/10,-2/10)
-        glVertex3f(0, 0.1, 0);  // XYZ=(0,-8/10,-2/10)
+        glVertex3f(0.425, -0.5, 0);
+        glVertex3f(0.45, -0.8, 0); 
+        glVertex3f(0, 0.1, 0);  
 
-        glVertex3f(0.425, -0.5,  0);  // XYZ=(-8/10,8/10,3/10)
-        glVertex3f(0.45, -0.8, 0);  // XYZ=(8/10,8/10,-2/10)
-        glVertex3f(0.9, 0.8, 0);  // XYZ=(0,-8/10,-2/10)
+        glVertex3f(0.425, -0.5, 0); 
+        glVertex3f(0.45, -0.8, 0);  
+        glVertex3f(0.9, 0.8, 0); 
 
-        glVertex3f(0.425, -0.5,  0);  // XYZ=(-8/10,8/10,3/10)
-        glVertex3f(0.75, 0.8, 0);  // XYZ=(8/10,8/10,-2/10)
-        glVertex3f(0.9, 0.8, 0);  // XYZ=(0,-8/10,-2/10)
+        glVertex3f(0.425, -0.5, 0); 
+        glVertex3f(0.75, 0.8, 0);
+        glVertex3f(0.9, 0.8, 0); 
     glEnd();
     glutSwapBuffers();
 }
@@ -46,6 +43,9 @@ int main(int argc, char **argv) {  // request double-buffered color window with 
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
     glutInit(&argc, argv);
     glutCreateWindow("Jacob Webb");
+    glShadeModel(GL_SMOOTH);  // smooth color interpolation
+    glEnable(GL_DEPTH_TEST);  // enable hidden surface removal
+    glClearColor(238/255.0, 118/255.0, 0, 1);
     glutDisplayFunc(display); // function to render window
     glutMainLoop();
 }
