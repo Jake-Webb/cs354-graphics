b3721cb5b3f440595cce332855a7d555a83e15bc
Jacob Webb <jakewebb4@utexas.edu>
2015-10-05 15:18:38 -0500

    going to algorithms

--- project3/geom.h
+++ project3/geom.h
@@ -1,3 +1,40 @@
+#include <vector>
+
+using namespace std;
+
+class Vertex {
+	public:
+		float x;
+		float y;
+		float z;
+
+		Vertex(float *vs) {
+			x = vs[0];
+			y = vs[1];
+			z = vs[2];
+		}
+};
+
+class Face {
+	public:
+		vector<Vertex> vs;
+};
+
 class Trimesh {
-	
-}
+	public:
+		vector<Vertex> vs;
+		vector<Face> fs;
+
+		Trimesh() {
+			
+		}
+
+		void addVertex(float *vs) {
+			Vertex v = Vertex(vs);
+			vs.push_back(v);
+		}
+
+		void addFace(int *vs) {
+
+		}
+};
